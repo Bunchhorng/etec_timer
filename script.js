0ba7b307acaad5792dd3e16620cdad05c2a7d790
Roeung Bunchhorng <roeungbunchhorng37@gmail.com>
2026-05-16 23:08:26 +0700

    third commit

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
 let countdown;
 
 // Alarm Sound
-const alarm = new Audio(
-  "./mp3/microsammy-clock-alarm-8761.mp3"
-);
+const alarm = new Audio("./mp3/microsammy-clock-alarm-8761.mp3");
 
 // Store alerted times
 let alertedTimes = [];
 
-function startExam(){
-
+function startExam() {
   let minutes = parseInt(document.getElementById("minutes").value);
 
-  if(minutes <= 0 || isNaN(minutes)){
+  if (minutes <= 0 || isNaN(minutes)) {
     alert("Please enter valid minutes");
     return;
   }
@@ -28,27 +25,26 @@ function startExam(){
   // Alert every 30 minutes
   let alertPoints = [];
 
-  for(let i = minutes - 1; i > 0; i -= 1){
+  for (let i = minutes - 30; i > 0; i -= 30) {
     alertPoints.push(i * 60);
   }
 
-  countdown = setInterval(function(){
-
+  countdown = setInterval(function () {
     let hrs = Math.floor(totalSeconds / 3600);
     let mins = Math.floor((totalSeconds % 3600) / 60);
     let secs = totalSeconds % 60;
 
-    hrs = hrs.toString().padStart(2, '0');
-    mins = mins.toString().padStart(2, '0');
-    secs = secs.toString().padStart(2, '0');
+    hrs = hrs.toString().padStart(2, "0");
+    mins = mins.toString().padStart(2, "0");
+    secs = secs.toString().padStart(2, "0");
 
-    document.getElementById("timer").innerHTML =
-      `${hrs} : ${mins} : ${secs}`;
+    document.getElementById("timer").innerHTML = `${hrs} : ${mins} : ${secs}`;
 
     // Check alert points
-    if(alertPoints.includes(totalSeconds) &&
-       !alertedTimes.includes(totalSeconds)){
-
+    if (
+      alertPoints.includes(totalSeconds) &&
+      !alertedTimes.includes(totalSeconds)
+    ) {
       alertedTimes.push(totalSeconds);
 
       let remainingMinutes = totalSeconds / 60;
@@ -58,12 +54,18 @@ function startExam(){
     }
 
     // Time Finished
-    if(totalSeconds <= 0){
-
+    if (totalSeconds <= 0) {
       clearInterval(countdown);
 
-      document.getElementById("timer").innerHTML =
-        "TIME UP";
+      let timer = document.getElementById("timer");
+
+      timer.innerHTML = "ដល់ម៉ោងហើយកូនៗ";
+      timer.style.fontSize = "90px";
+      timer.style.color = "red";
+      timer.style.fontWeight = "bold";
+
+      //animation class
+      timer.classList.add("blink-text");
 
       alarm.currentTime = 0;
       alarm.play();
@@ -72,6 +74,5 @@ function startExam(){
     }
 
     totalSeconds--;
-
   }, 1000);
 }
